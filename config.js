ec12633e53de92639a8ef4a1b83b4493110b773d
Matt Nelson <matt.nelson@autonoma.ai>
2026-05-13 12:27:44 -0700

    Happy 4th of july

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 window.APP_CONFIG = {
   SUPABASE_URL: 'https://vezpaumwwbkqukdkdsgs.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlenBhdW13d2JrcXVrZGtkc2dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2OTQ5NTYsImV4cCI6MjA5NDI3MDk1Nn0.kawFGEzpfW8cqoaViMZ4vZ2z0_oXRx1XSkoLhsA2fXc',
-  TMDB_API_KEY: 'YOUR-TMDB-API-KEY',
+  TMDB_API_KEY: 'd8eaeafabfdd4d71bb6b379f99927633',
 };
